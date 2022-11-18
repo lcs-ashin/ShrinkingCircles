0eec542cbed8e1616c6c2bcd75e9b5c80d979f3c
lcs-ashin <ashin@lcs.on.ca>
2022-11-18 14:58:28 -0500

    Got several circles to show up by repeating code manually.

--- ShrinkingCircles/ContentView.swift
+++ ShrinkingCircles/ContentView.swift
@@ -13,6 +13,43 @@ struct ShrinkingCircles: Shape {
         // Create the path
         var path = Path()
         
+        // First circle, largest
+        path.addEllipse(in: CGRect(origin: CGPoint(x: rect.midX - rect.height / 2, y: 0),
+                                   size: CGSize(width: rect.height,
+                                                height: rect.height)))
+        
+        // Second circle
+        path.addEllipse(in: CGRect(origin: CGPoint(x: rect.midX - rect.height * 0.9 / 2,
+                                                   y: rect.height * 0.05),
+                                   size: CGSize(width: rect.height * 0.9,
+                                                height: rect.height * 0.9)))
+        
+        
+        // Return the path
+        return path
+    }
+    
+    
+}
+
+struct ShrinkingCirclesIteration: Shape {
+    func path(in rect: CGRect) -> Path {
+        
+        // Create the path
+        var path = Path()
+        
+        // Create circles
+        for i in 0...10 {
+            
+            let ratio = 0.1 * Double(i)
+            let ratioTwo = 1 - ratio
+            
+            path.addEllipse(in: CGRect(origin: CGPoint(x: rect.midX - rect.height * ratioTwo / 2,
+                                                       y: rect.height * (ratio / 2)),
+                                       size: CGSize(width: rect.height * ratioTwo,
+                                                    height: rect.height * ratioTwo)))
+        }
+        
         
         
         // Return the path
@@ -24,7 +61,8 @@ struct ShrinkingCircles: Shape {
 
 struct ContentView: View {
     var body: some View {
-        ShrinkingCircles()
+        ShrinkingCirclesIteration()
+            .stroke(lineWidth: 3)
     }
 }
 
