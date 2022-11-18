22c4a378cd66052680782d58b3b9f41aa3527709
lcs-ashin <ashin@lcs.on.ca>
2022-11-18 14:19:02 -0500

    Initial Commit

---
+++ ShrinkingCircles/ShrinkingCirclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShrinkingCirclesApp.swift
+//  ShrinkingCircles
+//
+//  Created by Eunbi Shin on 2022-11-18.
+//
+
+import SwiftUI
+
+@main
+struct ShrinkingCirclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
